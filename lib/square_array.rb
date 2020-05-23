af9d450c40b3a718a8026f1c1fcedc8f5b582183
Penn M Diba <pdiba@oxbowschool.org>
2020-05-23 03:23:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   counter=0
-  length = array.length
   new_array=[]
-  while counter < length do
+  while square_array[counter] do
     new_array.push array[counter]**2 
     counter += 1
   end
